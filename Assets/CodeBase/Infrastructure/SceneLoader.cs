a64e708b1a0e41262ccb16a1e986a28c14dd9669
JanisZ1 <Jan4uks753@mail.ru>
2023-07-13 17:14:25 +0300

    Added active scene loading check

--- Assets/CodeBase/Infrastructure/SceneLoader.cs
+++ Assets/CodeBase/Infrastructure/SceneLoader.cs
@@ -17,6 +17,12 @@ namespace Assets.CodeBase.Infrastructure
 
         private IEnumerator LoadScene(string scene, Action onLoaded = null)
         {
+            if (SceneManager.GetActiveScene().name == scene)
+            {
+                onLoaded?.Invoke();
+                yield break;
+            }
+
             AsyncOperation progress = SceneManager.LoadSceneAsync(scene);
 
             while (!progress.isDone)
