3fd8362dcfe849599f2cc73080823e3371cce955
JanisZ1 <Jan4uks753@mail.ru>
2023-07-27 14:32:19 +0300

    CoinSpawnPointData generation

---
+++ Assets/CodeBase/Infrastructure/StaticData/CoinSpawnPointData.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace Assets.CodeBase.Infrastructure.StaticData
+{
+    public class CoinSpawnPointData
+    {
+        public Vector3 Position { get; private set; }
+
+        public CoinSpawnPointData(Vector3 position) =>
+            Position = position;
+    }
+}
