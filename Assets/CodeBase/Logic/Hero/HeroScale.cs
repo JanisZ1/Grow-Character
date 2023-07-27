ceec2abbb31690adf7ba80e893f96588bac24c22
JanisZ1 <Jan4uks753@mail.ru>
2023-07-27 13:11:35 +0300

    Logic to change heroScale depending on upgrades

--- Assets/CodeBase/Logic/Hero/HeroScale.cs
+++ Assets/CodeBase/Logic/Hero/HeroScale.cs
@@ -1,5 +1,7 @@
 ﻿using Assets.CodeBase.Infrastructure.Services.InputService;
+using Assets.CodeBase.Infrastructure.Services.Observer;
 using Assets.CodeBase.Infrastructure.Services.PlayerProgressService;
+using Assets.CodeBase.Infrastructure.StaticData;
 using UnityEngine;
 
 namespace Assets.CodeBase.Logic.Hero
@@ -7,27 +9,64 @@ namespace Assets.CodeBase.Logic.Hero
     public class HeroScale : MonoBehaviour
     {
         [SerializeField] private Transform _heroTransform;
-        [SerializeField] private float _scaleFactor;
 
         private IInputService _inputService;
         private IPlayerProgressService _playerProgress;
+        private IShopItemObserver _shopItemObserver;
 
-        public void Construct(IInputService inputService, IPlayerProgressService playerProgress)
+        private float _maximumMass;
+        private float _scaleFactor;
+
+        public void Construct(IInputService inputService, IPlayerProgressService playerProgress, IShopItemObserver shopItemObserver)
         {
             _inputService = inputService;
             _playerProgress = playerProgress;
+            _shopItemObserver = shopItemObserver;
         }
 
-        private void Start() =>
+        private void Start()
+        {
+            _shopItemObserver.Buyed += ChangeMaximumMass;
+            _shopItemObserver.Buyed += ChangeScaleFactor;
             _inputService.MouseButtonDown += AddScale;
+        }
 
-        private void OnDestroy() =>
+        private void OnDestroy()
+        {
             _inputService.MouseButtonDown -= AddScale;
+            _shopItemObserver.Buyed -= ChangeMaximumMass;
+            _shopItemObserver.Buyed -= ChangeScaleFactor;
+        }
+
+        private void ChangeMaximumMass(ShopItemData shopItemData) =>
+            _maximumMass = shopItemData.MaximumMass;
+
+        private void ChangeScaleFactor(ShopItemData shopItemData) =>
+            _scaleFactor = shopItemData.Calories;
 
         private void AddScale()
         {
-            _heroTransform.localScale += Vector3.one * _scaleFactor;
-            _playerProgress.PlayerProgress.MassData.Mass.Change(_heroTransform.localScale.x);
+            Vector3 massToChange = MassToChange();
+
+            if (massToChange.x < _maximumMass)
+            {
+                _heroTransform.localScale = massToChange;
+                SaveMassChange();
+            }
+            if (_heroTransform.localScale.x > _maximumMass)
+            {
+                _heroTransform.localScale = MaximumMass();
+                SaveMassChange();
+            }
         }
+
+        private void SaveMassChange() =>
+            _playerProgress.PlayerProgress.MassData.Mass.Change(_heroTransform.localScale.x);
+
+        private Vector3 MassToChange() =>
+            _heroTransform.localScale + Vector3.one * _scaleFactor;
+
+        private Vector3 MaximumMass() =>
+            new Vector3(_maximumMass, _maximumMass, _maximumMass);
     }
 }
