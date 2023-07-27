4b8132818616c3664da3611d439b9f8680071344
JanisZ1 <Jan4uks753@mail.ru>
2023-07-27 14:05:06 +0300

    CoinSpawnAreaEditor

---
+++ Assets/CodeBase/Editor/CoinSpawnAreaEditor.cs
@@ -0,0 +1,21 @@
+using Assets.CodeBase.Logic;
+using UnityEditor;
+using UnityEngine;
+
+namespace Assets.CodeBase.Editor
+{
+    [CustomEditor(typeof(CoinSpawnArea))]
+    public class CoinSpawnAreaEditor : UnityEditor.Editor
+    {
+        [DrawGizmo(GizmoType.Active | GizmoType.Pickable | GizmoType.NonSelected)]
+        public static void RenderCustomGizmo(CoinSpawnArea spawnArea, GizmoType gizmo)
+        {
+            Gizmos.color = new Color(1f, 0.962f, 0.016f, 0.3f);
+
+            BoxCollider[] boxColliders = spawnArea.GetComponents<BoxCollider>();
+
+            foreach (BoxCollider collider in boxColliders)
+                Gizmos.DrawCube(spawnArea.transform.position + collider.center, collider.size);
+        }
+    }
+}
