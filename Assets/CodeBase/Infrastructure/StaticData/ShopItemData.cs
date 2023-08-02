5dc2d79a69d8dbe87ac079e2973509b1e2d97188
JanisZ1 <Jan4uks753@mail.ru>
2023-08-02 13:02:29 +0300

    Added price to shopItemData

--- Assets/CodeBase/Infrastructure/StaticData/ShopItemData.cs
+++ Assets/CodeBase/Infrastructure/StaticData/ShopItemData.cs
@@ -5,6 +5,7 @@ namespace Assets.CodeBase.Infrastructure.StaticData
     [CreateAssetMenu(fileName = "ShopItemData", menuName = "StaticData/ShopItemData")]
     public class ShopItemData : ScriptableObject
     {
+        public float Price;
         public float Calories;
         public float MaximumMass;
 
