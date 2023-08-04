0163e0a45883232e067ad610cc95f721d73565dc
JanisZ1 <Jan4uks753@mail.ru>
2023-08-04 16:28:05 +0300

    Reduced maximumMass by 60 percent

--- Assets/CodeBase/Infrastructure/States/GameStates/LoadProgressState.cs
+++ Assets/CodeBase/Infrastructure/States/GameStates/LoadProgressState.cs
@@ -34,9 +34,9 @@ namespace Assets.CodeBase.Infrastructure.States.GameStates
         {
             PlayerProgress playerProgress = new PlayerProgress(bootstrapLevel: "Main");
 
-            playerProgress.MassData.Mass.Current = 0.1f;
-            playerProgress.MassData.MaxMass.Current = 0.2f;
-            playerProgress.MassData.Mass.ScaleFactor = 0.001f;
+            playerProgress.MassData.Mass.Current = 0.07f;
+            playerProgress.MassData.MaxMass.Current = 0.07f;
+            playerProgress.MassData.Mass.ScaleFactor = 0.0002f;
             playerProgress.MoneyData.ByClickEarnAmount = 0.008f;
 
             return playerProgress;
