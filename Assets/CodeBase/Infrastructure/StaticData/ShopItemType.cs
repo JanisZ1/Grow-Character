ffdc14c8c1b49d2602d3dde6ebf1977318ee52e1
JanisZ1 <Jan4uks753@mail.ru>
2023-08-04 14:38:09 +0300

    Added ShopItems to scriptable objects

--- Assets/CodeBase/Infrastructure/StaticData/ShopItemType.cs
+++ Assets/CodeBase/Infrastructure/StaticData/ShopItemType.cs
@@ -10,5 +10,13 @@
         Eggplant = 5,
         Carrot = 6,
         Apple = 7,
+        Banana = 8,
+        Pizza = 9,
+        Meat = 10,
+        Bread = 11,
+        Sausage = 12,
+        Donut = 13,
+        HotDog = 14,
+        Fries = 15,
     }
 }
