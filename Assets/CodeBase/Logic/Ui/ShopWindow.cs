d2473a119760a7e59474867a67beadd500c9e6a4
JanisZ1 <Jan4uks753@mail.ru>
2023-08-16 12:50:09 +0300

    Fixed shop dont saves last position of scroll

--- Assets/CodeBase/Logic/Ui/ShopWindow.cs
+++ Assets/CodeBase/Logic/Ui/ShopWindow.cs
@@ -1,10 +1,21 @@
-﻿using UnityEngine;
+﻿using Assets.CodeBase.Infrastructure.Data;
+using Assets.CodeBase.Infrastructure.Services.SaveLoad;
+using UnityEngine;
+using UnityEngine.UI;
 
 namespace Assets.CodeBase.Logic.Ui
 {
-    public class ShopWindow : MonoBehaviour
+    public class ShopWindow : MonoBehaviour, ISavedProgress
     {
+        [SerializeField] private Scrollbar _scrollBar;
+
         public void Close() =>
             Destroy(gameObject);
+
+        public void SaveProgress(PlayerProgress progress) =>
+            progress.ShopUiData.ScrollBarValue = _scrollBar.value;
+
+        public void LoadProgress(PlayerProgress progress) =>
+            _scrollBar.value = progress.ShopUiData.ScrollBarValue;
     }
 }
